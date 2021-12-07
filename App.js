6cfb9fa98cb0068255b9cef0f37cb46f46cac6d6
yuvalpoliak <81253836+yuvalpoliak@users.noreply.github.com>
2021-12-07 14:44:52 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,114 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// @refresh reset
+
+import React, { useState, useEffect, useCallback } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { StyleSheet, TextInput, View, YellowBox, Button, Pressable } from 'react-native'
+import * as firebase from 'firebase'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    //Your firebase config here
+    apiKey: "AIzaSyCmOmMcBsnY_0PzreiysaskViwWJm0jt2k",
+  authDomain: "firstexpoproject-83de3.firebaseapp.com",
+  projectId: "firstexpoproject-83de3",
+  storageBucket: "firstexpoproject-83de3.appspot.com",
+  messagingSenderId: "361288395792",
+  appId: "1:361288395792:web:034ac84c0014abd57a2e95",
+  measurementId: "G-S180G0NCPQ"
+
+}
+
+if (firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+YellowBox.ignoreWarnings(['Setting a timer for a long period of time'])
+
+const db = firebase.firestore()
+const chatsRef = db.collection('chats')
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [user, setUser] = useState(null)
+    const [name, setName] = useState('')
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        readUser()
+        const unsubscribe = chatsRef.onSnapshot((snapshot) => {
+            const messagesFirestore = snapshot
+                .docChanges
+                .filter(({ type }) => type === 'added')
+                .map(({ doc }) => {
+                    const message = doc.data()
+                    //createdAt is firebase.firestore.Timestamp instance
+                    //https://firebase.google.com/docs/reference/js/firebase.firestore.Timestamp
+                    return { ...message, createdAt: message.createdAt.getTime() }
+                })
+                .sort((a, b) => b.createdAt.getTime - a.createdAt.getTime)
+            appendMessages(messagesFirestore)
+        })
+        return () => unsubscribe()
+    }, [])
+
+    const appendMessages = useCallback(
+        (messages) => {
+            setMessages((previousMessages) => GiftedChat.append(previousMessages, messages))
+        },
+        [messages]
+    )
+
+    async function readUser() {
+        const user = await AsyncStorage.getItem('user')
+        if (user) {
+            setUser(JSON.parse(user))
+        }
+    }
+    async function handlePress() {
+        const _id = Math.random().toString(36).substring(7)
+        const user = { _id, name }
+        await AsyncStorage.setItem('user', JSON.stringify(user))
+        setUser(user)
+    }
+    async function handleSend(messages) {
+        const writes = messages.map((m) => chatsRef.add(m))
+        await Promise.all(writes)
+    }
+
+    if (!user) {
+        return (
+            <View style={styles.container}>
+                <TextInput style={styles.input} placeholder="Enter your name" value={name} onChangeText={setName} />
+                <Pressable style={styles.button} onPress={handlePress} title="Enter the chat" />
+            </View>
+        )
+    }
+    return <GiftedChat messages={messages} user={user} onSend={handleSend} />
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 30,
+    },
+    input: {
+        height: 50,
+        width: '100%',
+        borderWidth: 1,
+        padding: 15,
+        marginBottom: 20,
+        borderColor: 'blue',
+    },
+    button: {
+      borderColor: 'gray',
+      borderWidth: 1,
+      borderRadius: 20,
+      height: 25,
+      width: 75,
+      padding: 15,
+      margin: 20
+    }
+})
